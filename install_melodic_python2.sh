4615e063f15ec8c054fab7e0125976ae3746bf97
JungWKim <50034678+JungWKim@users.noreply.github.com>
2020-08-10 14:37:15 +0900

    Update install_melodic_python2.sh

--- install_melodic_python2.sh
+++ install_melodic_python2.sh
@@ -6,7 +6,7 @@ sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31
 sudo apt update
 sudo apt install -y ros-melodic-desktop-full
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-echo "alias cw="cd ~/catkin_ws"" >> ~/.bashrc
+echo "alias cw=\"cd ~/catkin_ws\"" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo apt install -y python-rosdep
@@ -17,3 +17,5 @@ cd ~/catkin_ws
 catkin_make
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+cd ~/catkin_ws
+catkin_make
